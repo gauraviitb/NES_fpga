4abcb133ea8dfdc276edd51073691cfd8dcd5ce8
Gaurav Rathi <thegauravrathi@gmail.com>
2022-03-25 09:20:54 +0530

    started project in ISE

---
+++ src/NES_fpga/RAM_64K.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    09:20:26 03/25/2022 
+// Design Name: 
+// Module Name:    RAM_64K 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module RAM_64K(
+    input clk,
+    input [15:0] addr,
+    inout [7:0] data,
+    input rst_n,
+    input rw_n,
+    input cs_n
+    );
+
+
+endmodule
---
+++ src/NES_fpga/cpu_6502.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    09:18:28 03/25/2022 
+// Design Name: 
+// Module Name:    cpu_6502 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module cpu_6502(
+    input clk,
+    input rst_n,
+    input [15:0] addr,
+    inout [7:0] data
+    );
+
+
+endmodule
